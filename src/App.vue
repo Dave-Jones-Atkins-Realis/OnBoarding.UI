dd7f1479593d08c6830d4a93aaeb8cd85dc58c01
David Jones <David.Jones5@atkinsrealis.com>
2024-08-14 14:29:37 +0100

    Added a crude list of notices and add / remove notice buttons

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
 import { ref } from 'vue';
-import { notices as noticeData, users as userData } from './data/dummydata.js';
+import dummyData from './data/dummydata.js';
 
-const notices = ref(noticeData);
-const users = ref(userData);
+const notices = ref(dummyData.notices);
+const users = ref(dummyData.users);
 const selectedNoticeId = ref(null);
 
 const getUserName = (authorId) => {
@@ -35,17 +35,34 @@ const deleteNoticeById = (id) => {
 
 <template>
   <header>
-
-    <div >
-      app.vue header
-    </div>
+    
   </header>
 
   <main>
-    app.vue main
+    <div class="center-horizontally">
+      
+      <img src="./assets/OnBoarding.png" alt="Notice Board" >
+      <h1>OnBoarding App</h1>
+    </div>
+
+    <ul>
+      <li v-for="notice in notices" :key="notice.Id">
+        {{notice}}
+      </li>
+    </ul>
+    <select v-model="selectedNoticeId" id="notices">
+      <option v-for="notice in notices" :key="notice.Id" :value="notice.Id">
+        {{getUserName(notice.Author) + ': ' + notice.Title}}
+      </option>  
+    </select>
+    <button @click="deleteNoticeById(selectedNoticeId)"> Remove</button>
+    <button @click="addNotice('some content')"> Add</button>
   </main>
 </template>
 
 <style scoped>
-
+  .center-horizontally {
+      margin: 0 auto;
+      width: 50%; /* Adjust as needed */
+  }
 </style>
