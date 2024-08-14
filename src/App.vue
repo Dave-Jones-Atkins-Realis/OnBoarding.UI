e249a89ba2c5d53c12396b9f7176a97659cc03e0
David Jones <David.Jones5@atkinsrealis.com>
2024-08-14 14:29:19 +0100

    Added a crude list of notices and add / remove notice buttons

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,36 @@
 <script setup>
+import { ref } from 'vue';
+import { notices as noticeData, users as userData } from './data/dummydata.js';
+
+const notices = ref(noticeData);
+const users = ref(userData);
+const selectedNoticeId = ref(null);
+
+const getUserName = (authorId) => {
+  const user = users.value.find(user => user.Id === authorId);
+  return user ? user.Name : 'Unknown';
+};
+
+const addNotice = (contents) => {
+  let newNotice = {
+    Id: notices.value.length + 1,
+    Author: 2,
+    Title: "New Notice Title",
+    Contents: contents,
+    TimeStamp: new Date().getTime()
+  };
+  notices.value.push(newNotice);
+  console.log(contents);
+  return true;
+};
+
+const deleteNoticeById = (id) => {
+  const index = notices.value.findIndex(notice => notice.Id === id);
+  if (index !== -1) {
+    notices.value.splice(index, 1);
+  }
+  return true;
+};
 </script>
 
 <template>
